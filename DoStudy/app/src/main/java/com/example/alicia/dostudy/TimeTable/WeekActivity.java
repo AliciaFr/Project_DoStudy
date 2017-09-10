84debaa2fe82f8b7492a38e6959dd7117a3a31d5
veraarev <vera.wittmann1@web.de>
2017-09-10 20:45:25 +0200

    rename & improve

--- DoStudy/app/src/main/java/com/example/alicia/dostudy/TimeTable/WeekActivity.java
+++ DoStudy/app/src/main/java/com/example/alicia/dostudy/TimeTable/WeekActivity.java
@@ -10,18 +10,18 @@ import com.example.alicia.dostudy.R;
 
 public class WeekActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button monday, tuesday, wednesday, thursday, friday;
-    Button moOne, moTwo, moThree, moFour, moFive, moSix;
-    Button tuOne, tuTwo, tuThree, tuFour, tuFive, tuSix;
-    Button weOne, weTwo, weThree, weFour, weFive, weSix;
-    Button thOne, thTwo, thThree, thFour, thFive, thSix;
-    Button frOne, frTwo, frThree, frFour, frFive, frSix;
+    Button mondayButton, tuesdayButton, wednesdayButton, thursdayButton, fridayButton;
+    Button moCourseOne, moCourseTwo, moCourseThree, moCourseFour, moCourseFive, moCourseSix;
+    Button tuCourseOne, tuCourseTwo, tuCourseThree, tuCourseFour, tuCourseFive, tuCourseSix;
+    Button weCourseOne, weCourseTwo, weCourseThree, weCourseFour, weCourseFive, weCourseSix;
+    Button thCourseOne, thCourseTwo, thCourseThree, thCourseFour, thCourseFive, thCourseSix;
+    Button frCourseOne, frCourseTwo, frCourseThree, frCourseFour, frCourseFive, frCourseSix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week);
-        setupDayFunctionality();
+        setupDays();
         setupMondayCourses();
         setupTuesdayCourses();
         setupWednesdayCourses();
@@ -29,117 +29,117 @@ public class WeekActivity extends AppCompatActivity implements View.OnClickListe
         setupFridayCourses();
     }
 
-    private void setupDayFunctionality(){
-        monday = (Button)findViewById(R.id.monday);
-        tuesday = (Button)findViewById(R.id.tuesday);
-        wednesday = (Button)findViewById(R.id.wednesday);
-        thursday = (Button)findViewById(R.id.thursday);
-        friday = (Button)findViewById(R.id.friday);
+    private void setupDays(){
+        mondayButton = (Button)findViewById(R.id.monday);
+        tuesdayButton = (Button)findViewById(R.id.tuesday);
+        wednesdayButton = (Button)findViewById(R.id.wednesday);
+        thursdayButton = (Button)findViewById(R.id.thursday);
+        fridayButton = (Button)findViewById(R.id.friday);
 
-        monday.setOnClickListener(this);
-        tuesday.setOnClickListener(this);
-        wednesday.setOnClickListener(this);
-        thursday.setOnClickListener(this);
-        friday.setOnClickListener(this);
+        mondayButton.setOnClickListener(this);
+        tuesdayButton.setOnClickListener(this);
+        wednesdayButton.setOnClickListener(this);
+        thursdayButton.setOnClickListener(this);
+        fridayButton.setOnClickListener(this);
     }
 
     private void setupMondayCourses(){
-        moOne = (Button)findViewById(R.id.mo_one);
-        moTwo = (Button)findViewById(R.id.mo_two);
-        moThree = (Button)findViewById(R.id.mo_three);
-        moFour = (Button)findViewById(R.id.mo_four);
-        moFive = (Button)findViewById(R.id.mo_five);
-        moSix = (Button)findViewById(R.id.mo_six);
+        moCourseOne = (Button)findViewById(R.id.mo_one);
+        moCourseTwo = (Button)findViewById(R.id.mo_two);
+        moCourseThree = (Button)findViewById(R.id.mo_three);
+        moCourseFour = (Button)findViewById(R.id.mo_four);
+        moCourseFive = (Button)findViewById(R.id.mo_five);
+        moCourseSix = (Button)findViewById(R.id.mo_six);
 
-        moOne.setOnClickListener(this);
-        moTwo.setOnClickListener(this);
-        moThree.setOnClickListener(this);
-        moFour.setOnClickListener(this);
-        moFive.setOnClickListener(this);
-        moSix.setOnClickListener(this);
+        moCourseOne.setOnClickListener(this);
+        moCourseTwo.setOnClickListener(this);
+        moCourseThree.setOnClickListener(this);
+        moCourseFour.setOnClickListener(this);
+        moCourseFive.setOnClickListener(this);
+        moCourseSix.setOnClickListener(this);
     }
 
     private void setupTuesdayCourses(){
-        tuOne = (Button)findViewById(R.id.tu_one);
-        tuTwo = (Button)findViewById(R.id.tu_two);
-        tuThree = (Button)findViewById(R.id.tu_three);
-        tuFour = (Button)findViewById(R.id.tu_four);
-        tuFive = (Button)findViewById(R.id.tu_five);
-        tuSix = (Button)findViewById(R.id.tu_six);
+        tuCourseOne = (Button)findViewById(R.id.tu_one);
+        tuCourseTwo = (Button)findViewById(R.id.tu_two);
+        tuCourseThree = (Button)findViewById(R.id.tu_three);
+        tuCourseFour = (Button)findViewById(R.id.tu_four);
+        tuCourseFive = (Button)findViewById(R.id.tu_five);
+        tuCourseSix = (Button)findViewById(R.id.tu_six);
 
-        tuOne.setOnClickListener(this);
-        tuTwo.setOnClickListener(this);
-        tuThree.setOnClickListener(this);
-        tuFour.setOnClickListener(this);
-        tuFive.setOnClickListener(this);
-        tuSix.setOnClickListener(this);
+        tuCourseOne.setOnClickListener(this);
+        tuCourseTwo.setOnClickListener(this);
+        tuCourseThree.setOnClickListener(this);
+        tuCourseFour.setOnClickListener(this);
+        tuCourseFive.setOnClickListener(this);
+        tuCourseSix.setOnClickListener(this);
     }
 
     private void setupWednesdayCourses(){
-        weOne = (Button)findViewById(R.id.we_one);
-        weTwo = (Button)findViewById(R.id.we_two);
-        weThree = (Button)findViewById(R.id.we_three);
-        weFour = (Button)findViewById(R.id.we_four);
-        weFive = (Button)findViewById(R.id.we_five);
-        weSix = (Button)findViewById(R.id.we_six);
+        weCourseOne = (Button)findViewById(R.id.we_one);
+        weCourseTwo = (Button)findViewById(R.id.we_two);
+        weCourseThree = (Button)findViewById(R.id.we_three);
+        weCourseFour = (Button)findViewById(R.id.we_four);
+        weCourseFive = (Button)findViewById(R.id.we_five);
+        weCourseSix = (Button)findViewById(R.id.we_six);
 
-        weOne.setOnClickListener(this);
-        weTwo.setOnClickListener(this);
-        weThree.setOnClickListener(this);
-        weFour.setOnClickListener(this);
-        weFive.setOnClickListener(this);
-        weSix.setOnClickListener(this);
+        weCourseOne.setOnClickListener(this);
+        weCourseTwo.setOnClickListener(this);
+        weCourseThree.setOnClickListener(this);
+        weCourseFour.setOnClickListener(this);
+        weCourseFive.setOnClickListener(this);
+        weCourseSix.setOnClickListener(this);
     }
 
     private void setupThursdayCourses(){
-        thOne = (Button)findViewById(R.id.th_one);
-        thTwo = (Button)findViewById(R.id.th_two);
-        thThree = (Button)findViewById(R.id.th_three);
-        thFour = (Button)findViewById(R.id.th_four);
-        thFive = (Button)findViewById(R.id.th_five);
-        thSix = (Button)findViewById(R.id.th_six);
+        thCourseOne = (Button)findViewById(R.id.th_one);
+        thCourseTwo = (Button)findViewById(R.id.th_two);
+        thCourseThree = (Button)findViewById(R.id.th_three);
+        thCourseFour = (Button)findViewById(R.id.th_four);
+        thCourseFive = (Button)findViewById(R.id.th_five);
+        thCourseSix = (Button)findViewById(R.id.th_six);
 
-        thOne.setOnClickListener(this);
-        thTwo.setOnClickListener(this);
-        thThree.setOnClickListener(this);
-        thFour.setOnClickListener(this);
-        thFive.setOnClickListener(this);
-        thSix.setOnClickListener(this);
+        thCourseOne.setOnClickListener(this);
+        thCourseTwo.setOnClickListener(this);
+        thCourseThree.setOnClickListener(this);
+        thCourseFour.setOnClickListener(this);
+        thCourseFive.setOnClickListener(this);
+        thCourseSix.setOnClickListener(this);
     }
 
     private void setupFridayCourses(){
-        frOne = (Button)findViewById(R.id.fr_one);
-        frTwo = (Button)findViewById(R.id.fr_two);
-        frThree = (Button)findViewById(R.id.fr_three);
-        frFour = (Button)findViewById(R.id.fr_four);
-        frFive = (Button)findViewById(R.id.fr_five);
-        frSix = (Button)findViewById(R.id.fr_six);
+        frCourseOne = (Button)findViewById(R.id.fr_one);
+        frCourseTwo = (Button)findViewById(R.id.fr_two);
+        frCourseThree = (Button)findViewById(R.id.fr_three);
+        frCourseFour = (Button)findViewById(R.id.fr_four);
+        frCourseFive = (Button)findViewById(R.id.fr_five);
+        frCourseSix = (Button)findViewById(R.id.fr_six);
 
-        frOne.setOnClickListener(this);
-        frTwo.setOnClickListener(this);
-        frThree.setOnClickListener(this);
-        frFour.setOnClickListener(this);
-        frFive.setOnClickListener(this);
-        frSix.setOnClickListener(this);
+        frCourseOne.setOnClickListener(this);
+        frCourseTwo.setOnClickListener(this);
+        frCourseThree.setOnClickListener(this);
+        frCourseFour.setOnClickListener(this);
+        frCourseFive.setOnClickListener(this);
+        frCourseSix.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.monday:
-                switchToDay("Montag", moOne, moTwo, moThree, moFour, moFive, moSix);
+                switchToDay("Montag", moCourseOne, moCourseTwo, moCourseThree, moCourseFour, moCourseFive, moCourseSix);
                 break;
             case R.id.tuesday:
-                switchToDay("Dienstag", tuOne, tuTwo, tuThree, tuFour, tuFive, tuSix);
+                switchToDay("Dienstag", tuCourseOne, tuCourseTwo, tuCourseThree, tuCourseFour, tuCourseFive, tuCourseSix);
                 break;
             case R.id.wednesday:
-                switchToDay("Mittwoch", weOne, weTwo, weThree, weFour, weFive, weSix);
+                switchToDay("Mittwoch", weCourseOne, weCourseTwo, weCourseThree, weCourseFour, weCourseFive, weCourseSix);
                 break;
             case R.id.thursday:
-                switchToDay("Donnerstag", thOne, thTwo, thThree, thFour, thFive, thSix);
+                switchToDay("Donnerstag", thCourseOne, thCourseTwo, thCourseThree, thCourseFour, thCourseFive, thCourseSix);
                 break;
             case R.id.friday:
-                switchToDay("Freitag", frOne, frTwo, frThree, frFour, frFive, frSix);
+                switchToDay("Freitag", frCourseOne, frCourseTwo, frCourseThree, frCourseFour, frCourseFive, frCourseSix);
                 break;
             default:
                 switchToCourse();
@@ -147,13 +147,13 @@ public class WeekActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void switchToDay(String day, Button one, Button two, Button three, Button four, Button five, Button six){
-        String courseOne = one.getText().toString();
-        String courseTwo = two.getText().toString();
-        String courseThree = three.getText().toString();
-        String courseFour = four.getText().toString();
-        String courseFive = five.getText().toString();
-        String courseSix = six.getText().toString();
+    private void switchToDay(String day, Button firstCourse, Button secondCourse, Button thirdCourse, Button fourthCourse, Button fifthCourse, Button sixthCourse){
+        String courseOne = firstCourse.getText().toString();
+        String courseTwo = secondCourse.getText().toString();
+        String courseThree = thirdCourse.getText().toString();
+        String courseFour = fourthCourse.getText().toString();
+        String courseFive = fifthCourse.getText().toString();
+        String courseSix = sixthCourse.getText().toString();
 
         Intent toDayIntent = new Intent(WeekActivity.this, DayActivity.class);
         toDayIntent.putExtra("dayName", day);
