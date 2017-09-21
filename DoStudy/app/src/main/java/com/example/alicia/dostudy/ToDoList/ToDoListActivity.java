94316de964ddb914b97e18abf601ee33410a9e1b
Nina <nina-eckart@gmx.de>
2017-09-21 21:17:11 +0200

    abc

--- DoStudy/app/src/main/java/com/example/alicia/dostudy/ToDoList/ToDoListActivity.java
+++ DoStudy/app/src/main/java/com/example/alicia/dostudy/ToDoList/ToDoListActivity.java
@@ -1,6 +1,5 @@
 package com.example.alicia.dostudy.ToDoList;
 
-import android.app.Activity;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
