56e6c1431e075c78ff0af9bc6a456d8b6e07fe6a
veraarev <vera.wittmann1@web.de>
2017-09-21 20:27:26 +0200

    database

--- DoStudy/app/src/main/java/com/example/alicia/dostudy/TimeTable/CourseItemDatabase.java
+++ DoStudy/app/src/main/java/com/example/alicia/dostudy/TimeTable/CourseItemDatabase.java
@@ -22,9 +22,20 @@ public class CourseItemDatabase {
     public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "course";
     public static final String KEY_BEGIN = "timeBegin";
+    public static final String KEY_END = "timeEnd";
+    public static final String KEY_ROOM = "room";
+    public static final String KEY_LECTURER = "lecturer";
+    public static final String KEY_TEST_DATE = "testDate";
+    public static final String KEY_COLOUR = "colour";
 
     public static final int COLUMN_NAME_INDEX = 1;
     public static final int COLUMN_BEGIN_INDEX = 2;
+    public static final int COLUMN_END_INDEX = 3;
+    public static final int COLUMN_ROOM_INDEX = 4;
+    public static final int COLUMN_LECTURER_INDEX = 5;
+    public static final int COLUMN_DATE_INDEX = 6;
+    public static final int COLUMN_COLOUR_INDEX = 7;
+
 
     private ToDoDBOpenHelper dbHelper;
     private SQLiteDatabase db;
@@ -49,6 +60,12 @@ public class CourseItemDatabase {
         ContentValues itemValues = new ContentValues();
         itemValues.put(KEY_NAME, item.getCourseName());
         itemValues.put(KEY_BEGIN, item.getCourseBegin());
+        itemValues.put(KEY_END, item.getCourseEnd());
+        itemValues.put(KEY_ROOM, item.getRoom());
+        itemValues.put(KEY_LECTURER, item.getLecturer());
+        itemValues.put(KEY_TEST_DATE, item.getTestDate());
+        itemValues.put(KEY_COLOUR, item.getColour());
+
         return db.insert(TABLE_NAME, null, itemValues);
     }
 
@@ -59,27 +76,36 @@ public class CourseItemDatabase {
     }
 
     public ArrayList<CourseItem> getAllCourseItems() {
-
         ArrayList<CourseItem> items = new ArrayList<CourseItem>();
-        Cursor cursor = db.query(TABLE_NAME, new String[] { KEY_ID, KEY_NAME, KEY_BEGIN }, null, null, null, null, null);
-
+        Cursor cursor = db.query(TABLE_NAME, new String[] { KEY_ID, KEY_NAME, KEY_BEGIN, KEY_END, KEY_ROOM, KEY_LECTURER, KEY_TEST_DATE, KEY_COLOUR }, null, null, null, null, null);
         if (cursor.moveToFirst()) {
             do {
                 String name = cursor.getString(COLUMN_NAME_INDEX);
                 String timeBegin = cursor.getString(COLUMN_BEGIN_INDEX);
+                String timeEnd = cursor.getString(COLUMN_END_INDEX);
+                String room = cursor.getString(COLUMN_ROOM_INDEX);
+                String lecturer = cursor.getString(COLUMN_LECTURER_INDEX);
+                String testDate = cursor.getString(COLUMN_DATE_INDEX);
+                String colour = cursor.getString(COLUMN_COLOUR_INDEX);
 
-                items.add(new CourseItem(name, timeBegin));
+                items.add(new CourseItem(name, timeBegin, timeEnd, room, lecturer, testDate, colour));
 
             } while (cursor.moveToNext());
         }
         return items;
     }
 
+
     private class ToDoDBOpenHelper extends SQLiteOpenHelper {
         private static final String DATABASE_CREATE = "create table "
                 + TABLE_NAME + " (" + KEY_ID + " integer primary key autoincrement, "
                 + KEY_NAME + " text not null, "
-                + KEY_BEGIN + " text not null);";
+                + KEY_BEGIN + " text not null, "
+                + KEY_END + " text not null, "
+                + KEY_ROOM + " text not null, "
+                + KEY_LECTURER + " text not null, "
+                + KEY_TEST_DATE + " text not null, "
+                + KEY_COLOUR + " integer not null);";
 
         public ToDoDBOpenHelper(Context c, String dbname, SQLiteDatabase.CursorFactory factory, int version) {
             super(c, dbname, factory, version);
