362ab1c0adbe0c380237232e944e482539ecfafe
veraarev <vera.wittmann1@web.de>
2017-09-22 19:49:53 +0200

    home_activity

--- DoStudy/app/src/main/java/com/example/alicia/dostudy/HomeActivity.java
+++ DoStudy/app/src/main/java/com/example/alicia/dostudy/HomeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,7 +15,7 @@ import com.example.alicia.dostudy.ToDoList.ToDoListActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ImageView toDo, timeTable, calendar, notesAndGrades;
+    private Button toDo, timeTable, calendar, notesAndGrades;
 
 
     @Override
@@ -26,10 +27,10 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void initUI() {
-        toDo = (ImageView) findViewById(R.id.home_todo);
-        calendar = (ImageView) findViewById(R.id.home_calendar);
-        timeTable = (ImageView) findViewById(R.id.home_timetable);
-        notesAndGrades = (ImageView) findViewById(R.id.home_notes_grades);
+        toDo = (Button) findViewById(R.id.home_todo);
+        calendar = (Button) findViewById(R.id.home_calendar);
+        timeTable = (Button) findViewById(R.id.home_timetable);
+        notesAndGrades = (Button) findViewById(R.id.home_notes_grades);
     }
 
     private void initClickListener() {
