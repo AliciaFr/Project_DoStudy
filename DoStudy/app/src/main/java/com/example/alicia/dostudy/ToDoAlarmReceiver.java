98ec59f2af31def43bb4eb360de1d04cdd347829
AliciaFr <alicia.fritsche@web.de>
2017-09-23 18:38:58 +0200

    Hinzufügen der Notifications
    Funktioniert leider noch nicht ganz

---
+++ DoStudy/app/src/main/java/com/example/alicia/dostudy/ToDoAlarmReceiver.java
@@ -0,0 +1,40 @@
+package com.example.alicia.dostudy;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+
+import com.example.alicia.dostudy.ToDoList.ToDoListActivity;
+
+
+public class ToDoAlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, ToDoListActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        Intent clickIntent = new Intent(context, ToDoListActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_todo_list_icon,
+                context.getResources().getString(R.string.notification_click_calendar), pendingIntent).build();
+
+        android.support.v4.app.NotificationCompat.Builder builder;
+        builder = new android.support.v4.app.NotificationCompat.Builder(context);
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setTicker(context.getResources().getString(R.string.notification_ticker_todo))
+                .setSmallIcon(R.drawable.ic_logo_owl)
+                .setContentTitle(context.getResources().getString(R.string.notification_title))
+                .setContentText(context.getResources().getString(R.string.notification_content_text_todo))
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .addAction(action);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+    }
+}
